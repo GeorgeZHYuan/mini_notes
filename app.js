31adc4a5be2b1500674297c49c5b7d5851ef039c
George Yuan <georgeyuan@Georges-MacBook-Pro.local>
2017-08-25 18:00:44 -0400

    First Commit

---
+++ app.js
@@ -0,0 +1,9 @@
+// console.log('Starting app.js');
+
+const fs = require('fs');
+const _ = require('lodash');
+
+const notes = require('./notes.js');
+
+console.log(_.isString(true));
+console.log(_.isString('George'));
---
+++ notes.js
@@ -0,0 +1,9 @@
+// console.log('Staring notes.js');
+// console.log(module);
+
+module.exports = {
+    addNote: () => {
+        console.log('addNote');
+        return 'New note';
+    },
+};
